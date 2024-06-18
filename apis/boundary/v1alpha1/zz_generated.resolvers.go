e8c80901968e4b181221714dd8b40347391db465
Vitalii <zebesh.vlad@gmail.com>
2024-06-18 15:38:47 +0700

    fix: add ref to managed groups to role

--- apis/boundary/v1alpha1/zz_generated.resolvers.go
+++ apis/boundary/v1alpha1/zz_generated.resolvers.go
@@ -10,6 +10,7 @@ import (
 	reference "github.com/crossplane/crossplane-runtime/pkg/reference"
 	resource "github.com/crossplane/upjet/pkg/resource"
 	errors "github.com/pkg/errors"
+	v1alpha1 "github.com/releaseband/crossplane-provider-boundary/apis/managed/v1alpha1"
 	client "sigs.k8s.io/controller-runtime/pkg/client"
 )
 
@@ -18,7 +19,24 @@ func (mg *Role) ResolveReferences(ctx context.Context, c client.Reader) error {
 	r := reference.NewAPIResolver(c, mg)
 
 	var rsp reference.ResolutionResponse
+	var mrsp reference.MultiResolutionResponse
 	var err error
+
+	mrsp, err = r.ResolveMultiple(ctx, reference.MultiResolutionRequest{
+		CurrentValues: reference.FromPtrValues(mg.Spec.ForProvider.PrincipalIds),
+		Extract:       resource.ExtractParamPath("id", true),
+		References:    mg.Spec.ForProvider.PrincipalIdsRefs,
+		Selector:      mg.Spec.ForProvider.PrincipalIdsSelector,
+		To: reference.To{
+			List:    &v1alpha1.GroupList{},
+			Managed: &v1alpha1.Group{},
+		},
+	})
+	if err != nil {
+		return errors.Wrap(err, "mg.Spec.ForProvider.PrincipalIds")
+	}
+	mg.Spec.ForProvider.PrincipalIds = reference.ToPtrValues(mrsp.ResolvedValues)
+	mg.Spec.ForProvider.PrincipalIdsRefs = mrsp.ResolvedReferences
 
 	rsp, err = r.Resolve(ctx, reference.ResolutionRequest{
 		CurrentValue: reference.FromPtrValue(mg.Spec.ForProvider.ScopeID),
